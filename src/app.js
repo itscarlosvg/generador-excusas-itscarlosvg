a9c11ca2d2bc8951e8032707cc45d6f95dd47f21
Carlos Vispo <151959952+itscarlosvg@users.noreply.github.com>
2025-10-23 15:31:39 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function getRandom(anyArray) {
+  return anyArray[Math.floor(Math.random() * anyArray.length)];
+}
+
+function generateExcuse() {
+  return getRandom(who) + " " + getRandom(action) + " " + getRandom(what) + " " + getRandom(when);
+}
+
+window.onload = function () {
+  console.log("Hello Rigo from the console!")
+  document.querySelector('#excuse').innerHTML = generateExcuse();
+
+  document.getElementById('generateBtn').addEventListener('click', function () {
+    document.querySelector('#excuse').innerHTML = generateExcuse();
+  });
 };
