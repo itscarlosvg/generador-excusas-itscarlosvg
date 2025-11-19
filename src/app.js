263e4fe4f113b6e25556145ecb0ea9b0562ba17d
Carlos Vispo <151959952+itscarlosvg@users.noreply.github.com>
2025-11-19 10:52:02 +0000

    refactor: cambiar let por const y usar template strings para mejorar legibilidad

--- src/app.js
+++ src/app.js
@@ -3,17 +3,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+const action = ['ate', 'peed', 'crushed', 'broke'];
+const what = ['my homework', 'my phone', 'the car'];
+const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 function getRandom(anyArray) {
   return anyArray[Math.floor(Math.random() * anyArray.length)];
 }
 
 function generateExcuse() {
-  return getRandom(who) + " " + getRandom(action) + " " + getRandom(what) + " " + getRandom(when);
+  return `${getRandomItem(who)} ${getRandomItem(action)} ${getRandomItem(what)} ${getRandomItem(when)}`;
 }
 
 window.onload = function () {
